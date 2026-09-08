39c9fbf14cf2abb547588c0f5dab39ed53f721db
yokyliu93-art <yokyliu93-art@users.noreply.github.com>
2026-09-09 00:22:19 +0800

    chore: 开放 dev/preview 到外部主机并支持测试 URL 注入

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,19 @@
 import {defineConfig} from 'vite';
 import {apiMiddleware} from './server/http.js';
-export default defineConfig({server:{host:'127.0.0.1',allowedHosts:['.lhr.life']},plugins:[{name:'island-api',configureServer(server){server.middlewares.use(apiMiddleware());},configurePreviewServer(server){server.middlewares.use(apiMiddleware());}}]});
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['.lhr.life', '.trycloudflare.com', '.loca.lt'],
+  },
+  plugins: [
+    {
+      name: 'island-api',
+      configureServer(server) {
+        server.middlewares.use(apiMiddleware());
+      },
+      configurePreviewServer(server) {
+        server.middlewares.use(apiMiddleware());
+      },
+    },
+  ],
+});
